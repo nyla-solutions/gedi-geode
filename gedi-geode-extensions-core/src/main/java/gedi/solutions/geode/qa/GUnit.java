d975ee16f0e84429b1510c2692636b388ea68a02
Gregory Green <green_gregory@yahoo.com>
2017-12-09 16:25:19 -0500

    Gunit changes

--- gedi-geode-extensions-core/src/main/java/gedi/solutions/geode/qa/GUnit.java
+++ gedi-geode-extensions-core/src/main/java/gedi/solutions/geode/qa/GUnit.java
@@ -3,8 +3,10 @@ package gedi.solutions.geode.qa;
 import java.nio.file.Paths;
 
 import org.apache.geode.cache.RegionShortcut;
+import org.apache.geode.distributed.LocatorLauncher;
 import gedi.solutions.geode.util.GemFireMgmt;
 import nyla.solutions.core.io.IO;
+import nyla.solutions.core.operations.ClassPath;
 import nyla.solutions.core.operations.Shell;
 import nyla.solutions.core.operations.Shell.ProcessInfo;
 import nyla.solutions.core.patterns.jmx.JMX;
@@ -14,31 +16,43 @@ import nyla.solutions.core.util.Debugger;
 
 public class GUnit
 {
-	private final String location = Config.getProperty("gfsh_location");
-	private final String runtimeDir = Config.getProperty("runtime_location","runtime");
+	private final static String location = Config.getProperty("gfsh_location");
+	private final static String runtimeDir = Config.getProperty("runtime_location","runtime");
+	private final static String locatorDir = runtimeDir+"/locator";
+	private final static String serverDir = runtimeDir+"/server";
+	
 	public GUnit()
 	{
 	}//------------------------------------------------
 	public void startCluster()
 	throws Exception
 	{
-		IO.mkdir(Paths.get(runtimeDir+"/locator").toFile());
-		IO.mkdir(Paths.get(runtimeDir+"/server").toFile());
+
 		
-		Shell shell = new Shell();
-		ProcessInfo pi = shell.execute(location+"/gfsh","-e","start locator  --dir=runtime/locator  --name=locator  --port=10334");
+		IO.mkdir(Paths.get(locatorDir).toFile());
+		IO.mkdir(Paths.get(serverDir).toFile());
+		
+		Shell locatorShell = new Shell(Paths.get(locatorDir).toFile(),
+		Paths.get(locatorDir+"/locator.start.log").toFile());
 		
 		
-		System.out.println(pi.exitValue);
-		System.out.println(pi.output);
-		System.out.println(pi.error);
+		ProcessInfo p = locatorShell.execute(true,"java","-classpath", ClassPath.getClassPathText(), LocatorLauncher.class.getName(),"start", "locator");
 		
-		 pi = shell.execute(location+"/gfsh",
+		if(p.hasError() && p.error.indexOf("locator is currently online") < 0)
+			throw new RuntimeException(p.error);
+		
+		System.out.println("OUT:"+p.output);
+		System.out.println("ERROR:"+p.error);
+		Thread.sleep(1000);
+		
+		Shell cacheServerShell = new Shell(Paths.get(serverDir).toFile(),
+		Paths.get(locatorDir+"/server.start.log").toFile());
+		 p = cacheServerShell.execute(true,location+"/gfsh",
 		"-e","start server --name=server --dir="+runtimeDir+"/server --locators=localhost[10334]"); 
-		 
-			System.out.println(pi.exitValue);
-			System.out.println("OUTPUT:"+pi.output);
-			System.out.println("ERROR:"+pi.error);
+		
+		 System.out.println("OUT:"+p.output);
+			System.out.println("ERROR:"+p.error);
+		 Thread.sleep(1000);
 	}
 	
 	public void createRegion(String regionName,RegionShortcut regionShortcut)
