c4712333ded700248a16d0bf0fd4daa0c71e6c97
Gregory Green <ggreen@pivotal.io>
2018-10-08 16:10:58 -0400

    Template for JAva based REST application

---
+++ gedi-geode-rest-app/src/main/java/gedi/solutions/geode/rest/GediGeodeRestAppApplication.java
@@ -0,0 +1,12 @@
+package gedi.solutions.geode.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GediGeodeRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GediGeodeRestAppApplication.class, args);
+	}
+}
